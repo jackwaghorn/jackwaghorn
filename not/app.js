0d553446ab01f2a7712feebfd13872b8c5f6a901
jackwaghorn <47627707+jackwaghorn@users.noreply.github.com>
2021-05-28 02:15:06 +0200

    poop

--- not/app.js
+++ not/app.js
@@ -1,11 +1,10 @@
-
 let blurLayer = document.getElementById("blur");
 let pageOverlay = document.getElementById("pageOverlay");
 
 let listKey = document.getElementById("list-key");
-let dotMap = document.querySelectorAll(".dot_resize");
+let dotMap = document.querySelectorAll(".dot");
 
-const dotMapContent = document.querySelector(".overlay").innerHTML;
+let dotMapContent = document.querySelector(".overlay").innerHTML;
 
 const datas = {
   red: "Window",
@@ -15,11 +14,8 @@ const datas = {
   orange: "Mezzanine lounge",
   blue: "Service Area",
   pink: "Fitting Rooms",
-  grey: "layout plans"
+  grey: "layout plans",
 };
-
-
-
 
 // Create key list
 for (const [key, value] of Object.entries(datas)) {
@@ -27,59 +23,36 @@ for (const [key, value] of Object.entries(datas)) {
   listKey.appendChild(li);
   li.innerHTML = `<div class="dot ${key}"></div>${value}`;
   li.id = key;
-};
+}
 
 let list = document.querySelectorAll("li");
 
 // Dot finders
-list.forEach(el => { dotChanger(el)});
-dotMap.forEach(el => {dotChanger(el)});
+list.forEach((el) => {
+  dotChanger(el);
+});
 
 // Select dot
 function dotChanger(el) {
-el.addEventListener("mouseover", function () {
-  let color = el.id;
-  document.querySelector(
-    ".overlay"
-  ).innerHTML = `<div class="dot ${color} ${color}_position dot_resize"></img>`;
+  el.addEventListener("mouseover", function () {
+    let color = el.id;
+    document.querySelector(
+      ".overlay"
+    ).innerHTML = `<div id="${color}" class="dot ${color} ${color}_position dot_resize"></img>`;
+  });
+  el.addEventListener("mouseout", function () {
+    document.querySelector(".overlay").innerHTML = dotMapContent;
+  });
+}
+
+list.forEach((i) => {
+  pageChanger(i);
 });
 
-el.addEventListener("mouseout", function () {
-  document.querySelector(".overlay").innerHTML = dotMapContent;
-});
-
-};
-
-// list.forEach(el => {dotRevert(el)});
-// dotMap.forEach(el => {dotRevert(el)});
-
-
-
-
-
-
-// List: Change back when mouse not hover
-
-// function dotRevert (el) {
-//   el.addEventListener("mouseout", function () {
-    // document.querySelector(".overlay").innerHTML = `        
-    //     <div class="dot yellow yellow_position dot_resize"></div>
-    //     <div class="dot blue blue_position dot_resize"></div>
-    //     <div class="dot pink pink_position dot_resize"></div>
-    //     <div class="dot grey grey_position dot_resize"></div>
-    //     <div class="dot green green_position dot_resize"></div>
-    //     <div class="dot red red_position dot_resize"></div>
-    //     <div class="dot orange orange_position dot_resize"></div>
-    //     <div class="dot teal teal_position dot_resize"></div>
-    //     `;
-//   })
-// };
-
-
-list.forEach(function (i) {
+function pageChanger(i) {
   i.addEventListener("click", function () {
     let color = i.id;
-    if (color === 'grey') {
+    if (color === "grey") {
       pageOverlay.innerHTML = `
     
     <div class="wrap">
@@ -87,31 +60,31 @@ list.forEach(function (i) {
         </div>
   
     `;
-    } else if (color === 'green' || color === 'yellow' || color === 'blue') {
-    let title = i.textContent;
-    pageOverlay.innerHTML = `
+    } else if (color === "green" || color === "yellow" || color === "blue") {
+      let title = i.textContent;
+      pageOverlay.innerHTML = `
     <div class="area">
         <div class="instagram"><img src="img/${color}/${color}_instagram.png" alt=""></div>
         <div class="moodboard"><img src="img/${color}/${color}_moodboard.png" alt=""></div>
         <div class="wireframe"><img src="img/${color}/${color}_wireframe.png" alt=""></div>
     </div>
     `;
-  } else {
-    let title = i.textContent;
-    pageOverlay.innerHTML = `
+    } else {
+      let title = i.textContent;
+      pageOverlay.innerHTML = `
     <div class="area">
         <div class="instagram2"><img src="img/${color}/${color}_instagram.png" alt=""></div>
         <div class="moodboard"><img src="img/${color}/${color}_moodboard.png" alt=""></div>
         <div class="wireframe"><img src="img/${color}/${color}_wireframe.png" alt=""></div>
     </div>
     `;
-  }});
-});
+    }
+  });
+}
 
-
-$("#list-key").click(function(){
-  $(pageOverlay).fadeIn( 300,"linear");
-  $(blurLayer).blur('blur-out', 300);
+$("#list-key").click(function () {
+  $(pageOverlay).fadeIn(300, "linear");
+  $(blurLayer).blur("blur-out", 300);
 });
 
 // $(dotMap).click(function(){
@@ -119,29 +92,28 @@ $("#list-key").click(function(){
 //   $(blurLayer).blur('blur-out', 300);
 // });
 
-
-$(pageOverlay).click(function(){
+$(pageOverlay).click(function () {
   $(pageOverlay).fadeOut(300);
-  $(blurLayer).blur('blur-in', 300);
- 
+  $(blurLayer).blur("blur-in", 300);
 });
 
-pageOverlay.addEventListener('click', function() {
-  blurLayer.classList.remove('blur-out');
-})
+pageOverlay.addEventListener("click", function () {
+  blurLayer.classList.remove("blur-out");
+});
 
-
-  $( ".loader" ).delay( 1000 ).fadeOut( 800, function() {
+$(".loader")
+  .delay(1500)
+  .fadeOut(1000, function () {
     // Animation complete.
   });
 
-
-
-pageOverlay.addEventListener('mouseover', function () {
-  document.querySelector('.moodboard').addEventListener('mouseover', function () {
-    document.querySelector('.moodboard').classList.add('.shadow');
+pageOverlay.addEventListener("mouseover", function () {
+  let moodboard = document.querySelector(".moodboard");
+  let wireframe = document.querySelector(".wireframe");
+  wireframe.addEventListener("mouseover", function () {
+    document.querySelector(".moodboard").style.filter = "blur(3px)";
   });
-  document.querySelector('.wireframe').addEventListener('mouseover', function () {
-    document.querySelector('.wireframe').classList.add('.shadow');
+  wireframe.addEventListener("mouseout", function () {
+    document.querySelector(".moodboard").style.filter = "blur(0px)";
   });
-})
+});
